8a09be0a126f63561375df61183422e951159c1a
eisc <eiscreme@posteo.de>
2018-08-01 21:58:28 +0200

    switched telegram library

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
-'use strict'
+const TelegramBot = require('node-telegram-bot-api')
 
-const Telegram = require('telegram-node-bot'),
-  tg = new Telegram.Telegram('TOKEN', {
-    workers: 1
-  });
+let config = require('./config')
+const token = config.TELEGRAM_TOKEN
 
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true})
 
-  class StartController extends Telegram.TelegramBaseController {
-    handle($){
-      $.sendMessage('This is the magic.');
-  }}
-  class VersionController extends Telegram.TelegramBaseController {
-    handle($){
-      $.sendChatAction('typing');
-      $.sendMessage('version 1.337');
+let globalStations = []
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id
+  const resp = match[1] // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp)
+})
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendChatAction(msg.chat.id, 'typing')
+  bot.sendMessage(msg.chat.id, 'Hallo ' + msg.from.first_name + '\n Ich helfe dir gerne bei den Abfahrtszeiten von Bussen und Bahnen der LVB.\n Bei /help werden dir alle Funktionen dieses Bots aufgelistet.')
+})
+
+bot.onText(/\/plan/, (msg) => {
+  bot.sendChatAction(msg.chat.id, 'upload_photo')
+  bot.sendPhoto(msg.chat.id, 'http://phototrans.de/images/schemas/original/71/914.jpg')
+  bot.sendDocument(msg.chat.id, '/home/eiscreme/fancy_LVB_Telegram_Bot/Netzplan_Tag.pdf')
+})
+
+bot.onText(/\/add (.+)/, (msg, match) => {
+  let parts = match[1]
+  globalStations.push(parts)
+  bot.sendMessage(msg.chat.id, 'something', {
+    'reply_markup': {
+      'keyboard': [globalStations]
     }
   }
-  class PlanController extends Telegram.TelegramBaseController {
-    handle($){
-      $.sendChatAction('upload_photo');
-      $.sendPhoto('http://phototrans.de/images/schemas/original/71/914.jpg');
-      $.sendDocument({ path: '/home/eiscreme/lvb_bot/Netzplan_Tag.pdf'})
-    }
-  }
+  )
+})
 
-  class OtherwiseController extends Telegram.TelegramBaseController {
-    handle($){
-      $.sendChatAction('typing');
-      $.sendMessage('Sorry, try again.');
-    }
-  }
+bot.onText(/\/reset (.*)/, (msg, match) => {
+  console.log(JSON.stringify(match));
+  let parts = match[1]
+  console.log('lhgerlk');
+})
 
-  const ListController = require('./list');
-  const listCtrl = new ListController();
-
-  tg.router.when(new Telegram.TextCommand('/start'), new StartController())
-    .when(new Telegram.TextCommand('/version'), new VersionController())
-    .when(new Telegram.TextCommand('/plan'), new PlanController())
-    .when(new Telegram.TextCommand('/list', 'listCommand'), listCtrl)
-    .when(new Telegram.TextCommand('/add', 'addCommand'), listCtrl)
-    .when(new Telegram.TextCommand('/remove', 'removeCommand'), listCtrl)
-    .otherwise(new OtherwiseController());
+bot.onText(/\/reset/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'something', {
+    'reply_markup': {
+      'remove_keyboard': true
+    }})
+})
+//
+// class OtherwiseController extends Telegram.TelegramBaseController {
+//   handle ($) {
+//     $.sendChatAction('typing')
+//     $.sendMessage('Sorry, try again.')
+//   }
+// }
+//
