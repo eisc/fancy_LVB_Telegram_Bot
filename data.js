e075ea85f10dbe218f5f3a710dade63c718cd3ed
Nucsum <nucsum@mailbox.org>
2018-08-22 22:53:41 +0200

    added function to the the next closest station, imported lodash

--- data.js
+++ data.js
@@ -1,15 +1,16 @@
 var Database = require('better-sqlite3');
+var sortBy = require('lodash/sortBy');
 var db = new Database('lvb.db');
 
 // find all stations with substring in name
 // getStationsByName('Lütz', 5);
 function getStationsByName(_name, _maxResults) {
     let sql = 'select stop_id, stop_name, stop_lat, stop_lon '
-            + 'from stops '
-            + 'where stop_name like \'%' + _name + '%\' '
+        + 'from stops '
+        + 'where stop_name like \'%' + _name + '%\' '
 
     if (_maxResults > 0) {
-        sql+= 'limit ' + _maxResults
+        sql += 'limit ' + _maxResults
     }
 
     let stmt = db.prepare(sql).all()
@@ -22,35 +23,30 @@ function getStationsByName(_name, _maxResults) {
 // getArrivalsByStopId('0013196', 0)
 function getArrivalsByStopId(_stopId, _maxResults) {
     let sql = 'select st.arrival_time, st.stop_id, s.stop_name, s.stop_lat, s.stop_lon '
-            + 'from stop_times st ' 
-            + 'inner join stops s on st.stop_id = s.stop_id '
-            + 'where st.stop_id = \'' + _stopId + '\' '
+        + 'from stop_times st '
+        + 'inner join stops s on st.stop_id = s.stop_id '
+        + 'where st.stop_id = \'' + _stopId + '\' '
 
     if (_maxResults > 0) {
-        sql+= 'limit ' + _maxResults
+        sql += 'limit ' + _maxResults
     }
-            
+
     let stmt = db.prepare(sql).all()
 
     console.dir(stmt);
     return stmt;
 }
 
-function getClosestStations() {
-    let sql = 'select stop_id, stop_name, stop_lat, stop_lon from stops limit 5'
-            
+// get the N closest stations next to your position
+// getClosestStations('51.326440', '12.342333', 8)
+function getClosestStations(_lat, _lon, _count) {
+    let sql = 'select stop_id, stop_name, stop_lat, stop_lon from stops'
     let stmt = db.prepare(sql).all()
 
-    let distance = 2
-    stmt.forEach(function(station){
-
-        distance++;
-        station.distance = distance;
-        console.log(station)
+    stmt.forEach(function (station) {
+        station.distance = Math.sqrt(Math.pow(_lat - station.stop_lat, 2) + Math.pow(_lon - station.stop_lon, 2));
     })
 
-    //console.dir(stmt);
-    return stmt;
+    let sortedByDistance = sortBy(stmt, ['distance'])
+    return sortedByDistance.slice(0,_count);
 }
-
-getClosestStations()
