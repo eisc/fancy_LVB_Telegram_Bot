8de648a790a7a06fefe52cce8477e4dc0b695ddd
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2018-10-20 21:44:25 +0200

    11 Abfrage der Abfahrten nach Stationsname

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api')
-// const departures = require('lvb').departures
+process.env["NTBA_FIX_319"] = 1;
+const TelegramBot = require('node-telegram-bot-api');
+const moment = require('moment');
+const stations = require('lvb').stations;
+const departures = require('lvb').departures;
 
 let config = require('./config')
 const token = config.TELEGRAM_TOKEN
@@ -149,4 +152,52 @@ bot.onText(/\/station(\s*)(.*)/, (msg, match) => {
   // let lat = lat.station
   // let lon = lon.station
   bot.sendVenue(msg.chat.id, 51.325209, 12.400980, `${match[2]}`, 'Linien:')
+})
+bot.onText(/\/departure(\s*)(.*)/, (msg, match) => {
+  if (match[2] === '') {
+    bot.sendMessage(msg.chat.id, 'Bitte gib eine Haltestelle ein.')
+    return
+  }
+  const stopName = match[2];
+  try {
+    stations(stopName).then(station => {
+      for(index in station) {
+        departures(station[index].id, new Date()).then(
+          departureResults => {
+            for(depIndex in departureResults) {
+              const res = departureResults[depIndex];
+              if(res.line) {
+                var answer = `Abfahrt von ${res.line.name} in Richtung ${res.line.direction}`;
+                if(res.timetable) {
+                  answer += '\n';
+                  for(timeIndex in res.timetable) {
+                    const time = res.timetable[timeIndex];
+                    const depTime = new Date(Date.parse(time.departure));
+                    const departureStr = moment(depTime).format('HH:mm:ss');
+                    answer += `- um ${departureStr}`;
+                    if(time.departureDelay != 0) {
+                      const delay = new Date(time.departureDelay);
+                      const delayStr = moment(delay).format('mm:ss');
+                      answer += ` mit einer Verspätung von ${delayStr} Minuten`;
+                    }
+                    answer += '\n';
+                  }
+                } else {
+                  answer += '- Keine Abfahrtszeiten verfügbar';
+                }
+                bot.sendMessage(msg.chat.id, answer);
+              } else {
+                bot.sendMessage(msg.chat.id, 'Keine Linieninformationen verfügbar');
+              }
+            }
+          }
+        ).catch(error => {
+          bot.sendMessage(msg.chat.id, 'Fehler ' + error)
+        })
+      }
+    });
+  } catch (error) {
+    bot.sendMessage(msg.chat.id, 'Fehler ' + error)
+  }  
+  return;
 })
