1d8a561dbdd3463bd88dea2a54673a4af0772438
eisc <eiscreme@posteo.de>
2018-10-24 19:28:55 +0200

    code cleanup

--- index.js
+++ index.js
@@ -142,36 +142,36 @@ bot.onText(/\/departure(\s*)(.*)/, (msg, match) => {
     bot.sendMessage(msg.chat.id, 'Bitte gib eine Haltestelle ein.')
     return
   }
-  const stopName = match[2];
+  const stopName = match[2]
   try {
     stations(stopName).then(station => {
-      for(index in station) {
+      for (index in station) {
         departures(station[index].id, new Date()).then(
           departureResults => {
-            for(depIndex in departureResults) {
-              const res = departureResults[depIndex];
-              if(res.line) {
-                var answer = `Abfahrt von ${res.line.name} in Richtung ${res.line.direction}`;
-                if(res.timetable) {
-                  answer += '\n';
-                  for(timeIndex in res.timetable) {
-                    const time = res.timetable[timeIndex];
-                    const depTime = new Date(Date.parse(time.departure));
-                    const departureStr = moment(depTime).format('HH:mm:ss');
-                    answer += `- um ${departureStr}`;
-                    if(time.departureDelay != 0) {
-                      const delay = new Date(time.departureDelay);
-                      const delayStr = moment(delay).format('mm:ss');
-                      answer += ` mit einer Verspätung von ${delayStr} Minuten`;
+            for (depIndex in departureResults) {
+              const res = departureResults[depIndex]
+              if (res.line) {
+                var answer = `Abfahrt von ${res.line.name} in Richtung ${res.line.direction}`
+                if (res.timetable) {
+                  answer += '\n'
+                  for (timeIndex in res.timetable) {
+                    const time = res.timetable[timeIndex]
+                    const depTime = new Date(Date.parse(time.departure))
+                    const departureStr = moment(depTime).format('HH:mm:ss')
+                    answer += `- um ${departureStr}`
+                    if (time.departureDelay !== 0) {
+                      const delay = new Date(time.departureDelay)
+                      const delayStr = moment(delay).format('mm:ss')
+                      answer += ` mit einer Verspätung von ${delayStr} Minuten`
                     }
-                    answer += '\n';
+                    answer += '\n'
                   }
                 } else {
-                  answer += '- Keine Abfahrtszeiten verfügbar';
+                  answer += '- Keine Abfahrtszeiten verfügbar'
                 }
-                bot.sendMessage(msg.chat.id, answer);
+                bot.sendMessage(msg.chat.id, answer)
               } else {
-                bot.sendMessage(msg.chat.id, 'Keine Linieninformationen verfügbar');
+                bot.sendMessage(msg.chat.id, 'Keine Linieninformationen verfügbar')
               }
             }
           }
@@ -179,9 +179,9 @@ bot.onText(/\/departure(\s*)(.*)/, (msg, match) => {
           bot.sendMessage(msg.chat.id, 'Fehler ' + error)
         })
       }
-    });
+    })
   } catch (error) {
     bot.sendMessage(msg.chat.id, 'Fehler ' + error)
-  }  
-  return;
+  }
+  return
 })
