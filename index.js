045a86aeb48a848c51b8eb916c4d48ae5dec9d0a
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2018-11-07 20:34:26 +0100

    promise refactoring

--- index.js
+++ index.js
@@ -142,46 +142,58 @@ bot.onText(/\/departure(\s*)(.*)/, (msg, match) => {
     bot.sendMessage(msg.chat.id, 'Bitte gib eine Haltestelle ein.')
     return
   }
-  const stopName = match[2]
-  try {
-    stations(stopName).then(station => {
-      for (index in station) {
-        departures(station[index].id, new Date()).then(
-          departureResults => {
-            for (depIndex in departureResults) {
-              const res = departureResults[depIndex]
-              if (res.line) {
-                var answer = `Abfahrt von ${res.line.name} in Richtung ${res.line.direction}`
-                if (res.timetable) {
-                  answer += '\n'
-                  for (timeIndex in res.timetable) {
-                    const time = res.timetable[timeIndex]
-                    const depTime = new Date(Date.parse(time.departure))
-                    const departureStr = moment(depTime).format('HH:mm:ss')
-                    answer += `- um ${departureStr}`
-                    if (time.departureDelay !== 0) {
-                      const delay = new Date(time.departureDelay)
-                      const delayStr = moment(delay).format('mm:ss')
-                      answer += ` mit einer Verspätung von ${delayStr} Minuten`
-                    }
-                    answer += '\n'
-                  }
-                } else {
-                  answer += '- Keine Abfahrtszeiten verfügbar'
-                }
-                bot.sendMessage(msg.chat.id, answer)
-              } else {
-                bot.sendMessage(msg.chat.id, 'Keine Linieninformationen verfügbar')
-              }
-            }
-          }
-        ).catch(error => {
-          bot.sendMessage(msg.chat.id, 'Fehler ' + error)
-        })
+  const stopName = match[2];
+  stations(stopName).then(stations => {
+    if(stations.length) {
+      stations.forEach(station => handleStation(msg, station))
+    } else {
+      bot.sendMessage(msg.chat.id, 'Keine Station gefunden für ' + stopName)
+    }
+  }).catch(error => {
+    bot.sendMessage(msg.chat.id, 'Fehler ' + error)
+  })
+})
+
+function handleDeparture(msg, station, departureResults) {
+  if(departureResults.length) {
+    departureResults.forEach(res => {
+      if(res.line) {
+        var answer = `Abfahrt ab ${station.name} von ${res.line.name} in Richtung ${res.line.direction}`;
+        if(res.timetable) {
+          answer += '\n';
+          res.timetable.forEach(time => {
+            answer += handleDepartureTime(time);
+          })
+        } else {
+          answer += '- Keine Abfahrtszeiten verfügbar für ' + station.name;
+        }
+        bot.sendMessage(msg.chat.id, answer);
+      } else {
+        bot.sendMessage(msg.chat.id, 'Keine Linieninformationen verfügbar');
       }
     })
-  } catch (error) {
-    bot.sendMessage(msg.chat.id, 'Fehler ' + error)
+  } else {
+    bot.sendMessage(msg.chat.id, 'Keine aktuellen Abfahrten gefunden für ' + station.name);
   }
-  return
-})
+}
+
+function handleStation(msg, station) {
+  departures(station.id, new Date()).then(
+    departure => handleDeparture(msg, station, departure)
+  ).catch(error => {
+    bot.sendMessage(msg.chat.id, 'Fehler ' + error)
+  })
+}
+
+function handleDepartureTime(time) {
+  const depTime = new Date(Date.parse(time.departure));
+  const departureStr = moment(depTime).format('HH:mm:ss');
+  var answer = `- um ${departureStr}`;
+  if(time.departureDelay != 0) {
+    const delay = new Date(time.departureDelay);
+    const delayStr = moment(delay).format('mm:ss');
+    answer += ` mit einer Verspätung von ${delayStr} Minuten`;
+  }
+  answer += '\n';
+  return answer;
+}
