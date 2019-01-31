1dda4cbd24c5f3e279967b7546a61dc0a0be085c
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2019-01-31 22:19:24 +0100

    Telegram token aus env oder js Datei oder dummy

--- app/helper/bot.js
+++ app/helper/bot.js
@@ -1,7 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api')
-const { TELEGRAM_TOKEN } = require('../../config/config')
+const telegramToken = getTelegramToken('TELEGRAM_TOKEN', '../../config/config')
 
 // Create a bot that uses 'polling' to fetch new updates
 exports.createBot = function () {
-    return new TelegramBot(TELEGRAM_TOKEN, { polling: true })
+    return new TelegramBot(telegramToken, { polling: true })
+}
+
+function getTelegramToken(envParam, file) {
+    try {
+        return process.env[envParam] || require(file).TELEGRAM_TOKEN;
+    } catch (error) {
+        return 'dummyToken'
+    }
 }
