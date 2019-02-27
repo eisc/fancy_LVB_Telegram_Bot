558d28c7710d4c6573edcbb4633ca648be4832db
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2019-02-27 21:19:31 +0100

    deploy script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/git/fancy_LVB_Telegram_Bot
+docker rm -f fancylvbbot
+git checkout master
+git pull
+docker build -t fancy_lvb_bot
+docker run -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --restart=always --name fancylvbbot -d fancy_lvb_bot:latest
