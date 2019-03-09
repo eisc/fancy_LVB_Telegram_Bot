c7efb6842f56ecd9088031436b1ff0f21bf7fc1e
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2019-03-09 22:55:57 +0100

    start script docker build korrigieren

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ cd ~/git/fancy_LVB_Telegram_Bot
 docker rm -f fancylvbbot
 git checkout master
 git pull
-docker build -t fancy_lvb_bot
+docker build -t fancy_lvb_bot .
 docker run -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --restart=always --name fancylvbbot -d fancy_lvb_bot:latest
